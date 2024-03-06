49e0600efdeff8a1521312170759b9813bc385bb
rnahill <rei333lly@gmail.com>
2024-03-06 16:10:56 -0500

    started signup page

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,12 +1,36 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 
-export default function SignupForm(){
+
+
+export default function SignupPage() {
 
 
 
     return (
-        <div>
-
+        <div className='signup-container'>
+            <Box
+                width={200}
+                my={4}
+                gap={4}
+                borderRadius={10}
+                p={5}
+                sx={{ border: '4px solid grey' }}
+                component="form"
+                noValidate
+                autoComplete="off"
+            >
+                <h2>Sign Up</h2>
+                <div className='signup-forms'>
+                    <TextField id="outlined-basic" label="First Name" variant="outlined" margin="dense" />
+                    <TextField id="outlined-basic" label="Last Name" variant="outlined" margin="dense" />
+                    <TextField id="outlined-basic" label="Email" variant="outlined" margin="dense" />
+                    <TextField id="outlined-basic" label="Password" variant="outlined" margin="dense" />
+                    <TextField id="outlined-basic" label="Confirm password" variant="outlined" margin="dense" />
+                </div>
+            </Box>
         </div>
     )
 }

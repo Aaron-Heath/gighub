4547532c607ef45238ade1936b7a3d160ebf5875
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-03-07 19:00:24 -0500

    started music bio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SignupPage from './pages/SignupPage'
+import MusicianBio from './pages/MusicianBio'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +11,10 @@ function App() {
   return (
     <>
       <main>
-      <SignupPage />
+      {/* <SignupPage /> */}
+      <MusicianBio />
       </main>
+  
     </>
   )
 }

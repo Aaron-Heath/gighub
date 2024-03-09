a40a797b32464845af7efa9449ba8686b0e086c7
rnahill <rei333lly@gmail.com>
2024-03-09 16:30:37 -0500

    changed to checkbox

--- client/src/components/MusicianForm/index.jsx
+++ client/src/components/MusicianForm/index.jsx
@@ -1,13 +1,33 @@
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import './style.css';
 
-export default function MusicianForm(){
+export default function MusicianForm() {
 
 
 
     return (
         <div>
-
+            <div className='musicianSignUp-container'>
+                <Box
+                    sx={{
+                        flexGrow: 1,
+                        alignContent: "center"
+                    }}
+                    component="form"
+                    noValidate
+                    autoComplete="off"
+                >
+                    <div className='musicianForms'>
+                    <TextField id="outlined-basic" label="First Name" variant="outlined" margin="dense" />
+                    <TextField id="outlined-basic" label="Last Name" variant="outlined" margin="dense" />
+                    <TextField id="outlined-basic" label="Email" variant="outlined" margin="dense" />
+                    <TextField id="outlined-basic" label="Password" variant="outlined" margin="dense" />
+                    <TextField id="outlined-basic" label="Confirm password" variant="outlined" margin="dense" />
+                    </div>
+                </Box>
+            </div>
         </div>
     )
 }
--- client/src/pages/SignupPage/index.jsx
+++ client/src/pages/SignupPage/index.jsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
+import { useState } from "react";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
+import MuscianForm from "../../components/MusicianForm"
 // import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import gighubLogo from "../../assets/images/Gighub-290px.png";
@@ -17,63 +14,104 @@ import "./style.css";
 export default function SignupPage() {
 
 
-// Code to handle radio color
-    const [selectedValue, setSelectedValue] = React.useState('a');
+    const [isChecked, setIsChecked] = useState(false);
 
-    const handleChange = (event) => {
-      setSelectedValue(event.target.value);
+    const handleOnChange = () => {
+        setIsChecked(!isChecked);
+
+
     };
-  
-    const controlProps = (item) => ({
-      checked: selectedValue === item,
-      onChange: handleChange,
-      value: item,
-      name: 'color-radio-button-demo',
-      inputProps: { 'aria-label': item },
-    });
-  
-//------------------------------------
 
-    return (
-
-        <div className='signup-container'>
-
-            <Box
-                sx={{
-                    flexGrow: 1,
-                    alignContent: "center"
-                }}
-                component="form"
-                noValidate
-                autoComplete="off"
-            >
-                <img src={gighubLogo} />
-                <div className='signup-forms'>
-                    <h2>Sign Up</h2>
-                    <TextField id="outlined-basic" label="First Name" variant="outlined" margin="dense" />
-                    <TextField id="outlined-basic" label="Last Name" variant="outlined" margin="dense" />
-                    <TextField id="outlined-basic" label="Email" variant="outlined" margin="dense" />
-                    <TextField id="outlined-basic" label="Password" variant="outlined" margin="dense" />
-                    <TextField id="outlined-basic" label="Confirm password" variant="outlined" margin="dense" />
-                    <div className='radio-container'>
-                        <FormControl>
-                            <FormLabel id="demo-radio-buttons-group-label">Are you a musician?</FormLabel>
-                            <RadioGroup
-                                
-                                aria-labelledby="demo-radio-buttons-group-label"
-                                defaultValue="No"
-                                name="radio-buttons-group"
-                            >
-                                <FormControlLabel value="Yes" control={<Radio {...controlProps('Yes')} color="default" />} label="Yes" />
-                                <FormControlLabel value="No" control={<Radio {...controlProps('No')} color="default"/>} label="No" />
-
-                            </RadioGroup>
-                        </FormControl>
+    if (isChecked) {
+        return (
+            <div className='signup-container'>
+                <Box
+                    sx={{
+                        flexGrow: 1,
+                        alignContent: "center"
+                    }}
+                    component="form"
+                    noValidate
+                    autoComplete="off"
+                >
+                    <img src={gighubLogo} />
+                    <div className='signup-forms'>
+                        <h2>Sign Up</h2>
+                        <p>Are you a musician?</p>
+                        <input
+                            type="checkbox"
+                            id="yesCheck"
+                            name="yesCheck"
+                            value="Yes"
+                            checked={isChecked}
+                            onChange={handleOnChange}
+                        />
+                        <label for="yesCheck">Yes</label><br />
+                        <input
+                            type="checkbox"
+                            id="noCheck"
+                            name="noCheck"
+                            value="No"
+                        />
+                        <label for="noCheck">No</label>
+                        <TextField id="outlined-basic" label="First Name" variant="outlined" margin="dense" />
+                        <TextField id="outlined-basic" label="Last Name" variant="outlined" margin="dense" />
+                        <TextField id="outlined-basic" label="Email" variant="outlined" margin="dense" />
+                        <TextField id="outlined-basic" label="Password" variant="outlined" margin="dense" />
+                        <TextField id="outlined-basic" label="Confirm password" variant="outlined" margin="dense" />
                     </div>
-                </div>
+                    
+                </Box>
+                <MuscianForm />
                 <Button variant="outlined" id="signupBtn">Submit</Button>
-            </Box>
+            </div>
+        )
+    } else {
 
-        </div>
-    )
+        return (
+
+            <div className='signup-container'>
+
+                <Box
+                    sx={{
+                        flexGrow: 1,
+                        alignContent: "center"
+                    }}
+                    component="form"
+                    noValidate
+                    autoComplete="off"
+                >
+                    <img src={gighubLogo} />
+                    <div className='signup-forms'>
+                        <h2>Sign Up</h2>
+                        <p>Are you a musician?</p>
+                        <input
+                            type="checkbox"
+                            id="yesCheck"
+                            name="yesCheck"
+                            value="Yes"
+                            checked={isChecked}
+                            onChange={handleOnChange}
+                        />
+                        <label for="yesCheck"> Yes</label><br />
+                        <input
+                            type="checkbox"
+                            id="noCheck"
+                            name="noCheck"
+                            value="No"
+                        />
+                        <label for="noCheck"> No</label>
+                        <TextField id="outlined-basic" label="First Name" variant="outlined" margin="dense" />
+                        <TextField id="outlined-basic" label="Last Name" variant="outlined" margin="dense" />
+                        <TextField id="outlined-basic" label="Email" variant="outlined" margin="dense" />
+                        <TextField id="outlined-basic" label="Password" variant="outlined" margin="dense" />
+                        <TextField id="outlined-basic" label="Confirm password" variant="outlined" margin="dense" />
+
+                    </div>
+                    <Button variant="outlined" id="signupBtn">Submit</Button>
+                </Box>
+
+            </div>
+        )
+    }
 }

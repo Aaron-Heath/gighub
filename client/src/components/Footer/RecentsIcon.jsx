d996ac7ec2f2f939cc29c14a6427d7d8441b8fe4
rnahill <rei333lly@gmail.com>
2024-03-10 13:10:30 -0400

    created bottom navigation

---
+++ client/src/components/Footer/RecentsIcon.jsx
@@ -0,0 +1,8 @@
+import recents from "../../assets/images/recents.png";
+import "./style.css"
+
+export default function RecentsIcon() {
+
+    return <img src={recents} className="recents-icon"/>
+    
+}
--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,12 +1,55 @@
 import './style.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { BottomNavigation } from '@mui/material';
+import { BottomNavigationAction } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/system';
+import FavoritesIcon from './FavoritesIcon';
+import SearchIcon from './SearchIcon';
+import RecentsIcon from './RecentsIcon';
+import "./style.css"
+// import RestoreIcon from '@mui/icons-material/Restore';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-export default function Footer(){
+export default function Footer() {
+  const [value, setValue] = React.useState(0);
+
+  const theme = createTheme({
+    palette: {
+      textColor: { darkRed: '#711F31' },
+      background: { yellow: '#FFE5A1' }
+    },
+  });
 
 
 
-    return (
-        <div>
-
-        </div>
-    )
+  return (
+    <ThemeProvider theme={theme}>
+    <Box sx={{ 
+        width: "100%",
+        position: 'fixed', 
+        bottom: 0, 
+        left: 0, 
+        right: 0,
+        }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        sx={{ backgroundColor: "background.yellow" }}
+        
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        
+        
+      >
+        <BottomNavigationAction label="Recents" icon={<RecentsIcon />} sx={{ color:"textColor.darkRed" }} />
+        <BottomNavigationAction label="Favorites" icon={<FavoritesIcon />} sx={{ color:"textColor.darkRed" }} />
+        <BottomNavigationAction label="Search" icon={<SearchIcon />} sx={{ color:"textColor.darkRed" }} />
+      </BottomNavigation>
+    </Box>
+    </ThemeProvider>
+  );
 }

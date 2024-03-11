789f99912e96b15ef4edab3334e218336ac64c5a
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-03-11 19:34:40 -0400

    added a little animate to settings title

--- client/src/pages/AccountSettings/index.jsx
+++ client/src/pages/AccountSettings/index.jsx
@@ -5,6 +5,8 @@ import React from 'react';
 import { Box, TextField, Button } from '@mui/material';
 import gighubLogo from "../../assets/images/Gighub-290px.png";
 import './style.css';
+import { settingVariants } from "./indexVariants";
+import { motion } from 'framer-motion'
 
 
 export default function AccountSettings(){
@@ -13,7 +15,7 @@ export default function AccountSettings(){
     return (
         <div>
             <div>
-                <img src={gighubLogo} alt="Logo" />
+                {/* <img src={gighubLogo} alt="Logo" /> */}
                 <Box
                     className="settings-container"
                     gap={4}
@@ -22,29 +24,39 @@ export default function AccountSettings(){
                         borderRadius: 2,
                         flexGrow: 1,
                         alignContent: "center",
+                        marginTop: '10px'
                     }}
                     component="form"
                     noValidate
                     autoComplete="off"
                 >
-                    <h2>Settings</h2>
                     
-                    {/* Change/Update Stage Name */}
-                    <TextField className="form" id="stageName" label=" Stage Name" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1', borderRadius: '10px' }}/>
+                    <motion.div
+  className="settings"
+  variants={settingVariants}
+//   initial="animate"
+  animate='animate'
+  whileHover='whileHover'
+  style={{ marginTop: '60px', color: 'white' }}
+>
+  Settings
+</motion.div>
+     
+                   
+            
+                    
+                <TextField className="form" id="stageName" label=" Stage Name" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1', borderRadius: '10px', width: '80%', marginBottom: '10px', marginTop: '100px'}}/>
 
-                    {/* Change/Update Public Email */}
-                    <TextField className="form" id="publicEmail" label=" Email" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px'}}/>
+                    
+                    <TextField className="form" id="publicEmail" label=" Email" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px', width: '80%', marginBottom: '10px'}}/>
 
-                    {/* Change/Update City */}
-                    <TextField className="form" id="city" label="City" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px'}} />
+                  
+                    <TextField className="form" id="city" label="City" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px', width: '80%', marginBottom: '10px'}} />
 
-                    {/* Change/Update State */}
-                    <TextField className="form" id="state" label="State" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px'}} />
+                    <TextField className="form" id="state" label="State" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px', width: '80%', marginBottom: '10px'}} />
 
-                    {/* Change/Update Image Link */}
-                    <TextField className="form" id="imageLink" label="Image Link" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px'}} />
+                    <TextField className="form" id="imageLink" label="Image Link" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px', width: '80%', marginBottom: '10px'}} />
 
-                    {/* Change/Update Description */}
                     <TextField
                         className="form"
                         id="description"
@@ -52,14 +64,13 @@ export default function AccountSettings(){
                         multiline
                         rows={4}
                         variant="outlined"
-                        style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px'}}
+                        style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px', width: '80%', marginBottom: '10px'}}
                     />
 
-                    {/* Choose from Tags */}
-                    <TextField className="form" id="tags" label="Choose from Tags" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px'}} />
+                    <TextField className="form" id="tags" label="Search for Tags" variant="outlined" style={{backgroundColor: "#711F31", color: "#FFE5A1", border:'2px solid #FFE5A1',borderRadius: '10px', width: '80%', marginBottom: '10px'}} />
 
                     <div className='save-button'>
-                        <Button variant="contained" style={{ backgroundColor: "#711F31", color: "#FFE5A1", borderRadius: '10px'}}>
+                        <Button variant="contained" style={{ backgroundColor: "#711F31", color: "#FFE5A1", borderRadius: '10px', marginBottom: '10px'}}>
                             Save Changes
                         </Button>
                     </div>
